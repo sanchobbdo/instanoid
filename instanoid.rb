c35633c4530e438ca1dcfb2da9eea662e39df895
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-04-17 11:18:05 -0500

    init instagram in script

--- instanoid.rb
+++ instanoid.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'yaml'
+require 'instagram'
 
 puts "Instanoid is running."
 
@@ -13,3 +14,7 @@ print "\n"
 config.each_pair { |k, v| puts "#{k}: #{v}" }
 print "\n"
 
+Instagram.configure do |c|
+  c.client_id = config['client_id']
+  c.client_secret = config['client_secret']
+end

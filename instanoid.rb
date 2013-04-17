8bd4c333e3ab3c0ac9a6d01d2b5f6f5aab2dc634
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-04-17 11:42:44 -0500

    instanoid polling loop

--- instanoid.rb
+++ instanoid.rb
@@ -18,3 +18,20 @@ Instagram.configure do |c|
   c.client_id = config['client_id']
   c.client_secret = config['client_secret']
 end
+
+tag = config['tag']
+
+puts "Asking instagram about the last posted under ##{tag}"
+min_tag_id = Instagram.tag_recent_media(tag).pagination.min_tag_id
+puts "min_tag_id: #{min_tag_id}"
+
+puts "Start polling ..."
+begin
+  data = Instagram.tag_recent_media(tag, :min_tag_id => min_tag_id)
+
+  min_tag_id = data.pagination.min_tag_id
+
+  puts "Sleeping (20s)..."
+  sleep(20)
+  puts "Waking up."
+end while true

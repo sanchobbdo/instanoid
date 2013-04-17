903e6e2fa1e8b1ba33357808f9966a1badf1d646
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-04-17 10:56:26 -0500

    use config.yml

---
+++ instanoid.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'yaml'
+
+puts "Instanoid is running."
+
+print "Reading config.yml..."
+config = YAML.load_file('config.yml')
+print "Done.\n"
+
+print "\n"
+config.each_pair { |k, v| puts "#{k}: v" }
+print "\n"
+
+

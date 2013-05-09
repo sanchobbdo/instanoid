77af85f8d95a7bcb8b690d95a955869dc9196015
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-05-09 11:37:55 -0500

    use prawn_renderer

--- instanoid.rb
+++ instanoid.rb
@@ -10,7 +10,7 @@ require 'open-uri'
 require 'cupsffi'
 
 require 'instanoid'
-require 'erb_renderer'
+require 'prawn_renderer'
 
 puts "Instanoid is running."
 
@@ -53,7 +53,7 @@ begin
 
   entries.each_with_index do |entry, i|
     print "Rendering entry #{i}..."
-    filename = ErbRenderer.render(entry, Instanoid.renderer)
+    filename = PrawnRenderer.render(entry, Instanoid.renderer)
     print " Done.\n"
 
     print "Printing entry #{i}..."
